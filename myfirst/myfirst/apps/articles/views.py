b56357d965ca22a28abd76b2640e0efdbb13bf0c
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-08-04 16:36:27 +0300

    Fixed error

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.http import Http404, HttpResponseRedirect
 
 from django.shortcuts import render
-from .models import Article, Comment
+from .models import Article
 from django.urls import reverse
 
 
@@ -13,21 +13,22 @@ def index(request):
 def test(request):
     return HttpResponse('Hello, world!')
 
+
 def index(request):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
     return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
 
 
-
 def detail(request, article_id):
     try:
-        a = Article.objects.get(id = article_id)
+        a = Article.objects.get(id=article_id)
     except:
-        raise Http404('Articles is not exists(((')
-    return render(request, 'articles/detail.html', {'article': a})
-
+        raise Http404('Статья НЕ найдена(((')
     latest_comment_list = a.comment_set.order_by('-id')[:10]
     return render(request, 'articles/detail.html', {'article': a, 'latest_comment_list': latest_comment_list})
+
+    # return render(request, 'articles/detail.html', {'article': a})
+
 
 
 def leave_comment(request, article_id):
@@ -36,5 +37,5 @@ def leave_comment(request, article_id):
     except:
         raise Http404('Articles is not exists')
 
-    a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
-    return HttpResponseRedirect(reverse('articles:detail', args = (a.id,)))
+    a.comment_set.create(author_name=request.POST['name'], coment_text = request.POST['text'])
+    return HttpResponseRedirect(reverse('articles:detail', args=(a.id,)))
